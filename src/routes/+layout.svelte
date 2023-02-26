fef7ebb7697bb3156978f8bfa472b021509e6013
Cabidge <cabbageboi69@gmail.com>
2023-02-26 14:44:36 -0500

    ~style navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,20 @@
-<script>
+<script lang="ts">
     import SmartLink from '@lib/SmartLink.svelte';
     import '../app.postcss';
+
+    const routes = Object.entries({
+        Packs: '/pack'
+    });
 </script>
 
-<nav>
-    <SmartLink href="/pack">packs</SmartLink>
+<nav class="bg-slate-200 flex pl-2 item-stretch gap-2">
+    {#each routes as route}
+        <SmartLink
+            fixedStyle="hover:bg-slate-300 h-full p-2"
+            selectedStyle="bg-slate-400 hover:bg-slate-400"
+            href={route[1]}>{route[0]}</SmartLink
+        >
+    {/each}
 </nav>
 
 <slot />
