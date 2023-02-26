ccf5783db50a3b401a5c0925281231ee9afe7c6f
Cabidge <cabbageboi69@gmail.com>
2023-02-26 14:47:51 -0500

    +rework CreatePack to use Modal

--- src/lib/CreatePack.svelte
+++ src/lib/CreatePack.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import type { PackCreate } from '@bindings/PackCreate';
     import { invoke } from '@tauri-apps/api';
+    import Modal from './Modal.svelte';
     import { packs } from './packs';
 
     let active = false;
 
     let title = '';
-
-    let creating: Promise<void> = Promise.resolve();
 
     $: {
         if (!active) {
@@ -15,7 +14,7 @@
         }
     }
 
-    const _submit = async () => {
+    const submit = async () => {
         active = false;
 
         const pack: PackCreate = { title };
@@ -23,22 +22,16 @@
 
         packs.reload();
     };
-
-    const submit = () => {
-        creating = _submit();
-    };
 </script>
 
-{#await creating}
-    <p>creating...</p>
-{:then}
-    {#if active}
+<button on:click={() => (active = true)}>create pack...</button>
+
+{#if active}
+    <Modal>
         <form on:submit|preventDefault={submit}>
             <input type="text" bind:value={title} />
             <button type="submit">+</button>
             <button type="button" on:click={() => (active = false)}>cancel</button>
         </form>
-    {:else}
-        <button on:click={() => (active = true)}>create pack...</button>
-    {/if}
-{/await}
+    </Modal>
+{/if}
