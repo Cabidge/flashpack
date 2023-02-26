02b13c05be5473ec2f41f8004cb96df59241514a
Cabidge <cabbageboi69@gmail.com>
2023-02-26 14:23:13 -0500

    ~tweak styling

--- src/lib/PackSelector.svelte
+++ src/lib/PackSelector.svelte
@@ -16,8 +16,8 @@
             class="flex items-stretch"
         >
             <SmartLink
-                fixedStyle="w-full shadow text-center bg-gray-100 hover:bg-gray-200 font-semibold py-1 px-2 rounded"
-                selectedStyle="bg-gray-300 hover:bg-gray-300"
+                fixedStyle="w-full shadow text-center bg-slate-100 hover:bg-slate-200 font-semibold py-1 px-2 rounded"
+                selectedStyle="bg-blue-500 hover:bg-blue-600 text-white"
                 href="/pack/{pack.id}"
                 {disabled}>{pack.title}</SmartLink
             >
