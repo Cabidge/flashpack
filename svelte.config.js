50efa650f44e48f7e6301b51bbb28300bd43c266
Cabidge <cabbageboi69@gmail.com>
2023-02-25 22:04:24 -0500

    ~prerender auto

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,9 @@ const config = {
         })
     ],
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            fallback: "200.html"
+        }),
         alias: {
             '@lib/*': './src/lib/*',
             '@bindings/*': './src/bindings/*'
