889e5606237de7bb0fbd36e977295f3e660ba40b
Cabidge <cabbageboi69@gmail.com>
2023-02-25 20:40:26 -0500

    ~tweaked pack pages to not use layout data

--- src/routes/pack/[id]/+page.svelte
+++ src/routes/pack/[id]/+page.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-    import type { PageData } from './$types';
     import { page } from '$app/stores';
+    import { loadPack } from '@lib/pack';
 
-    export let data: PageData;
-
-    $: pack = data.pack;
-    $: cards = data.cards;
+    let cardPack = loadPack($page.params.id);
 </script>
 
-<h1>{pack.title} <a href="{$page.url.pathname}/edit">edit</a></h1>
+{#await cardPack}
+    <p>Loading...</p>
+{:then cardPack}
+    <h1>{cardPack.pack.title} <a href="{$page.url.pathname}/edit">edit</a></h1>
 
-<h2>Cards ({cards.length}) <a href="{$page.url.pathname}/add">add</a></h2>
+    <h2>Cards ({cardPack.cards.length}) <a href="{$page.url.pathname}/add">add</a></h2>
 
-{#if cards.length > 1}
-    <a href="{$page.url.pathname}/practice">begin practice</a>
-{:else}
-    <p>Add more cards to begin practicing</p>
-{/if}
+    {#if cardPack.cards.length > 1}
+        <a href="{$page.url.pathname}/practice">begin practice</a>
+    {:else}
+        <p>Add more cards to begin practicing</p>
+    {/if}
+{/await}
