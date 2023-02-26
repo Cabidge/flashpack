2ee11872424ad6e6c45a187f195710900c6e4e87
Cabidge <cabbageboi69@gmail.com>
2023-02-26 14:19:23 -0500

    ~style pack selector

--- src/lib/PackSelector.svelte
+++ src/lib/PackSelector.svelte
@@ -8,10 +8,19 @@
     export let disabled = false;
 </script>
 
-<ul>
+<ul class="flex flex-col gap-2 items-stretch m-2">
     {#each packs as pack (pack.id)}
-        <li transition:fade={{ duration: 150 }} animate:flip={{ duration: 200 }}>
-            <SmartLink href="/pack/{pack.id}" {disabled}>{pack.title}</SmartLink>
+        <li
+            transition:fade={{ duration: 150 }}
+            animate:flip={{ duration: 200 }}
+            class="flex items-stretch"
+        >
+            <SmartLink
+                fixedStyle="w-full shadow text-center bg-gray-100 hover:bg-gray-200 font-semibold py-1 px-2 rounded"
+                selectedStyle="bg-gray-300 hover:bg-gray-300"
+                href="/pack/{pack.id}"
+                {disabled}>{pack.title}</SmartLink
+            >
         </li>
     {/each}
 </ul>
