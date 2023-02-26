2990e898a48d01a2f7757ea7ca7b7a85fd1f9ac3
Cabidge <cabbageboi69@gmail.com>
2023-02-25 21:45:29 -0500

    ~tweak

--- src/routes/pack/[id]/+page.svelte
+++ src/routes/pack/[id]/+page.svelte
@@ -1,22 +1,16 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
     import { page } from '$app/stores';
     import { loadPack, type CardPack } from '@lib/pack';
+    import type { PageData } from './$types';
 
-    let cardPack: CardPack | undefined;
+    export let data: PageData;
 
-    $: {
-        (async () => {
-            try {
-                cardPack = await loadPack($page.params.id);
-            } catch {
-                await goto('/pack');
-            }
-        })();
-    }
+    $: id = data.id;
+
+    $: cardPack = loadPack(id);
 </script>
 
-{#if cardPack !== undefined}
+{#await cardPack then cardPack}
     <h1>{cardPack.pack.title} <a href="{$page.url.pathname}/edit">edit</a></h1>
 
     <h2>Cards ({cardPack.cards.length}) <a href="{$page.url.pathname}/add">add</a></h2>
@@ -26,4 +20,6 @@
     {:else}
         <p>Add more cards to begin practicing</p>
     {/if}
-{/if}
+{:catch}
+    <p>Pack not found...</p>
+{/await}
