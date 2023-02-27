351248a2a2d2c2b25e14361cb275eae2bad6629c
Cabidge <cabbageboi69@gmail.com>
2023-02-27 14:38:11 -0500

    +pack_update and pack_delete commands

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -2,6 +2,7 @@ import type { Card } from '@bindings/Card';
 import type { CardAdd } from '@bindings/CardAdd';
 import type { Pack } from '@bindings/Pack';
 import type { PackCreate } from '@bindings/PackCreate';
+import type { PackUpdate } from '@bindings/PackUpdate';
 import type { invoke as invoke_ } from '@tauri-apps/api';
 
 type Window = {
@@ -16,21 +17,29 @@ export const invoke: typeof invoke_ = async (cmd, args) => {
 };
 
 export const createPack = (pack: PackCreate) => {
-    return invoke<void>("create_pack", { pack });
-}
+    return invoke<void>('create_pack', { pack });
+};
 
 export const listPacks = () => {
-    return invoke<Pack[]>("list_packs");
-}
+    return invoke<Pack[]>('list_packs');
+};
 
 export const getPack = (id: string) => {
-    return invoke<Pack>("get_pack", { id });
-}
+    return invoke<Pack>('get_pack', { id });
+};
+
+export const deletePack = (id: string) => {
+    return invoke<void>('delete_pack', { id });
+};
+
+export const updatePack = (update: PackUpdate) => {
+    return invoke<void>('update_pack', { update });
+};
 
 export const listCards = (id: string) => {
-    return invoke<Card[]>("list_cards", { id });
-}
+    return invoke<Card[]>('list_cards', { id });
+};
 
 export const addCard = (card: CardAdd) => {
-    return invoke<void>("add_card", { card });
-}
+    return invoke<void>('add_card', { card });
+};
