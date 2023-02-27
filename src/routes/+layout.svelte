e162a782379ad9794886b1662d446c37550674e4
Cabidge <cabbageboi69@gmail.com>
2023-02-26 20:35:40 -0500

    ~restyle navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,13 +8,13 @@
 </script>
 
 <div class="h-screen flex flex-col">
-    <nav class="bg-slate-200 flex pl-2 item-stretch gap-2">
+    <nav class="bg-slate-300 flex pl-2 item-stretch gap-2">
         {#each routes as route}
             <SmartLink
                 styling={{
-                    base: "h-full p-2",
-                    unselected: "hover:bg-slate-300",
-                    selected: "bg-slate-400 hover:bg-slate-400",
+                    base: "px-4 m-2 rounded-full",
+                    unselected: "hover:bg-slate-200",
+                    selected: "bg-white hover:bg-slate-100",
                 }}
                 href={route[1]}
             >
