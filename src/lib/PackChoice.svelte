6fd17f560a046a29e4bb93289bf6ccb2e81bdd87
Cabidge <cabbageboi69@gmail.com>
2023-02-27 22:43:29 -0500

    ~removed need for UrlMatch component in PackChoice

--- src/lib/PackChoice.svelte
+++ src/lib/PackChoice.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
     import { goto, invalidateAll } from '$app/navigation';
+    import { page } from '$app/stores';
     import type { Pack } from '@bindings/Pack';
     import { deletePack } from './commands';
     import { conditionalClass, type ConditionalClass } from './styling';
-    import UrlMatch from './UrlMatch.svelte';
 
     export let pack: Pack;
 
     let hovering = false;
-
-    $: href = `/pack/${pack.id}`
 
     const remove = async () => {
         await deletePack(pack.id);
@@ -18,28 +16,29 @@
         await invalidateAll();
     };
 
-    const linkClass: ConditionalClass = {
+    $: href = `/pack/${pack.id}`
+    $: selected = $page.url.pathname.startsWith(href);
+
+    $: linkClass = conditionalClass(selected, {
         base: 'relative w-full font-semibold py-1 px-4 rounded',
         on: 'bg-indigo-500 hover:bg-indigo-600 shadow text-white',
         off: 'hover:bg-slate-200',
-    };
+    })
 </script>
 
-<UrlMatch {href} strict={false} let:selected>
-    <a 
-        {href}
-        class={conditionalClass(selected, linkClass)}
-        on:mouseenter={() => (hovering = true)}
-        on:mouseleave={() => (hovering = false)}
-    >
-        {pack.title}
-        {#if hovering}
-            <button
-                class="absolute right-2 top-1 bottom-1 hover:bg-black hover:bg-opacity-20 aspect-square rounded-full"
-                on:click|preventDefault={remove}
-            >
-                X
-            </button>
-        {/if}
-    </a>
-</UrlMatch>
+<a 
+    {href}
+    class={linkClass}
+    on:mouseenter={() => (hovering = true)}
+    on:mouseleave={() => (hovering = false)}
+>
+    {pack.title}
+    {#if hovering}
+        <button
+            class="absolute right-2 top-1 bottom-1 hover:bg-black hover:bg-opacity-20 aspect-square rounded-full"
+            on:click|preventDefault={remove}
+        >
+            X
+        </button>
+    {/if}
+</a>
