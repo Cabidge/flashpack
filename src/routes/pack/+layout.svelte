d883c54dff8df3f27fe78663135bf262ccd21198
Cabidge <cabbageboi69@gmail.com>
2023-02-28 12:39:33 -0500

    +ModalContext component for more flexibility in Modal construction

--- src/routes/pack/+layout.svelte
+++ src/routes/pack/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import CreatePack from '@lib/CreatePack.svelte';
+    import ModalContext from '@lib/ModalContext.svelte';
     import PackSelector from '@lib/PackSelector.svelte';
     import Transition from '@lib/Transition.svelte';
     import type { LayoutData } from './$types';
@@ -11,8 +12,19 @@
 
 <div class="flex flex-row h-full">
     <div class="w-1/3 max-w-[16rem] p-4 float-left overflow-x-hidden shadow flex flex-col gap-3">
-        <CreatePack />
-        <div class="border-b-2"></div>
+        <ModalContext let:open let:close>
+            <button
+                class="rounded bg-indigo-500 text-white hover:bg-indigo-600 text-center w-full py-2 shadow font-semibold"
+                on:click={open}
+            >
+                Create Pack
+            </button>
+
+            <CreatePack slot="modal" on:close={close} />
+        </ModalContext>
+
+        <div class="border-b-2" />
+
         <PackSelector {packs} />
     </div>
 
