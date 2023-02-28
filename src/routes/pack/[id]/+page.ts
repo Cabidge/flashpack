8d4ab00dcd236a4ea8ee00c667a21e1ffc1e0762
Cabidge <cabbageboi69@gmail.com>
2023-02-27 22:25:01 -0500

    ~404 error for /pack/[id]

--- src/routes/pack/[id]/+page.ts
+++ src/routes/pack/[id]/+page.ts
@@ -1,14 +1,19 @@
 import { getPack, listCards } from '@lib/commands';
+import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params }) => {
     const id = params.id;
 
-    const pack = await getPack(id);
-    const cards = await listCards(id);
+    try {
+        const pack = await getPack(id);
+        const cards = await listCards(id);
 
-    return {
-        pack,
-        cards
-    };
+        return {
+            pack,
+            cards
+        };
+    } catch {
+        throw error(404, "Not found");
+    }
 };
