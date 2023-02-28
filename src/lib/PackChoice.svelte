e02bbd5daa498979dd94e79a50a1bc387daf8f36
Cabidge <cabbageboi69@gmail.com>
2023-02-27 22:23:10 -0500

    ~rework PackChoice styling

--- src/lib/PackChoice.svelte
+++ src/lib/PackChoice.svelte
@@ -2,11 +2,14 @@
     import { goto, invalidateAll } from '$app/navigation';
     import type { Pack } from '@bindings/Pack';
     import { deletePack } from './commands';
-    import SmartLink from './SmartLink.svelte';
+    import { conditionalClass, type ConditionalClass } from './styling';
+    import UrlMatch from './UrlMatch.svelte';
 
     export let pack: Pack;
 
     let hovering = false;
+
+    $: href = `/pack/${pack.id}`
 
     const remove = async () => {
         await deletePack(pack.id);
@@ -14,28 +17,29 @@
 
         await invalidateAll();
     };
+
+    const linkClass: ConditionalClass = {
+        base: 'relative w-full font-semibold py-1 px-4 rounded',
+        on: 'bg-indigo-500 hover:bg-indigo-600 shadow text-white',
+        off: 'hover:bg-slate-200',
+    };
 </script>
 
-<div
-    class="w-full flex items-stretch relative"
-    on:mouseenter={() => (hovering = true)}
-    on:mouseleave={() => (hovering = false)}
->
-    <SmartLink
-        href="/pack/{pack.id}"
-        strict={false}
-        styling={{
-            base: 'w-full font-semibold py-1 px-4 rounded',
-            unselected: 'hover:bg-slate-200',
-            selected: 'bg-indigo-500 hover:bg-indigo-600 shadow text-white'
-        }}
+<UrlMatch {href} strict={false} let:selected>
+    <a 
+        {href}
+        class={conditionalClass(selected, linkClass)}
+        on:mouseenter={() => (hovering = true)}
+        on:mouseleave={() => (hovering = false)}
     >
         {pack.title}
         {#if hovering}
             <button
-                class="absolute right-3 top-0 bottom-0 hover:bg-slate-300"
-                on:click|preventDefault={remove}>del</button
+                class="absolute right-3 top-0 bottom-0 hover:bg-black hover:bg-opacity-10"
+                on:click|preventDefault={remove}
             >
+                del
+            </button>
         {/if}
-    </SmartLink>
-</div>
+    </a>
+</UrlMatch>
---
+++ src/lib/styling.ts
@@ -0,0 +1,9 @@
+export type ConditionalClass = {
+    base?: string,
+    on?: string,
+    off?: string,
+};
+
+export const conditionalClass = (condition: boolean, config: ConditionalClass) => {
+    return `${config.base ?? ''} ${condition ? (config.on ?? '') : (config.off ?? '')}`;
+}
