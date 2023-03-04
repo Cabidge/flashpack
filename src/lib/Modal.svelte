99982031b521b7637aa03dbd4a1f62b0d24bd959
Cabidge <cabbageboi69@gmail.com>
2023-03-04 18:31:25 -0500

    -remove preventDefault from Modal events
    will deal with this later

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -8,18 +8,18 @@
 {#if active}
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div
-        on:click|preventDefault={() => (active = false)}
+        on:click={() => (active = false)}
         transition:fade={{ duration: 100 }}
         class="fixed left-0 top-0 z-10 flex h-screen w-screen items-center justify-center overflow-auto bg-black bg-opacity-50"
     >
         <div
-            on:click|stopPropagation|preventDefault
+            on:click|stopPropagation
             transition:fly={{ y: 50, duration: 380, easing: expoOut }}
             class="relative rounded bg-white p-6 shadow-lg"
         >
             <button
                 class="absolute top-0 right-2 hover:font-bold"
-                on:click|preventDefault={() => (active = false)}
+                on:click={() => (active = false)}
             >
                 x
             </button>
