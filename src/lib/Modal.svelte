858bb2450e8a324fc3d92306978e087c9fff45c9
Cabidge <cabbageboi69@gmail.com>
2023-03-09 12:06:10 -0500

    +export open/close functions on Modal

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -3,12 +3,15 @@
     import { expoOut } from 'svelte/easing';
 
     export let active = false;
+
+    export const open = () => (active = true);
+    export const close = () => (active = false);
 </script>
 
 {#if active}
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div
-        on:click={() => (active = false)}
+        on:click={close}
         transition:fade={{ duration: 100 }}
         class="fixed left-0 top-0 z-10 flex h-screen w-screen items-center justify-center overflow-auto bg-black bg-opacity-50"
     >
@@ -17,12 +20,7 @@
             transition:fly={{ y: 50, duration: 380, easing: expoOut }}
             class="relative rounded bg-white p-6 shadow-lg"
         >
-            <button
-                class="absolute top-0 right-2 hover:font-bold"
-                on:click={() => (active = false)}
-            >
-                x
-            </button>
+            <button class="absolute top-0 right-2 hover:font-bold" on:click={close}> x </button>
 
             <slot />
         </div>
