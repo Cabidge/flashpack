27f3616de2219f7e40aef1de284bcd90a8ee8edc
Cabidge <cabbageboi69@gmail.com>
2023-03-12 15:18:59 -0400

    ~refactor ContextMenu +change ContextMenu to use position: fixed instead of absolute

--- src/lib/ContextMenu.svelte
+++ src/lib/ContextMenu.svelte
@@ -4,21 +4,13 @@
     const id = Symbol();
 
     export const select = (x: number, y: number) => menuStatus.open({ id, x, y });
-
-    let menuEl: HTMLDivElement;
-
-    $: selected = $menuStatus?.id === id;
-
-    $: x = $menuStatus?.x ?? 0;
-    $: y = $menuStatus?.y ?? 0;
 </script>
 
-{#if selected}
+{#if $menuStatus !== null && $menuStatus.id === id}
     <div
-        bind:this={menuEl}
-        class="absolute rounded bg-white p-1 shadow"
-        style:top="{y}px"
-        style:left="{x}px"
+        class="fixed rounded bg-white p-1 shadow"
+        style:top="{$menuStatus.y}px"
+        style:left="{$menuStatus.x}px"
     >
         <slot />
     </div>
