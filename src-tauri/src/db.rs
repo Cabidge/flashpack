3600aa6388447613fd242f7fa931865499d07030
Cabidge <cabbageboi69@gmail.com>
2023-03-16 17:45:57 -0400

    +create_tag and add_tag db functions

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -4,6 +4,7 @@ use sqlx::{SqlitePool, Row};
 
 pub type PackId = i32;
 pub type CardId = i32;
+pub type TagId = i32;
 
 pub async fn create_pack(pool: &SqlitePool, title: &str) -> Result<PackId> {
     let row = sqlx::query(
@@ -39,4 +40,36 @@ pub async fn add_card(pool: &SqlitePool, pack_id: PackId, front: &str, back: &st
     let id = row.try_get::<i32, _>("id")?;
 
     Ok(id)
+}
+
+pub async fn create_tag(pool: &SqlitePool, label: &str) -> Result<TagId> {
+    let row = sqlx::query(
+        "
+        INSERT INTO tags (label)
+        VALUES (?)
+        RETURNING id
+        ",
+    )
+    .bind(label)
+    .fetch_one(pool)
+    .await?;
+
+    let id = row.try_get::<i32, _>("id")?;
+
+    Ok(id)
+}
+
+pub async fn add_tag(pool: &SqlitePool, card_id: CardId, tag_id: TagId) -> Result<()> {
+    sqlx::query(
+        "
+        INSERT INTO card_tags (card_id, tag_id)
+        VALUES (?, ?)
+        ",
+    )
+    .bind(card_id)
+    .bind(tag_id)
+    .execute(pool)
+    .await?;
+
+    Ok(())
 }
