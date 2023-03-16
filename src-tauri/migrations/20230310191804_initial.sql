a8be4720d45afdbc7eb3e95e3cedb4ab229d4b24
Cabidge <cabbageboi69@gmail.com>
2023-03-15 21:14:00 -0400

    +add on tags to initial sql

--- src-tauri/migrations/20230310191804_initial.sql
+++ src-tauri/migrations/20230310191804_initial.sql
@@ -8,4 +8,21 @@ CREATE TABLE cards (
     front TEXT NOT NULL,
     back TEXT NOT NULL,
     pack_id INTEGER NOT NULL REFERENCES packs(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE tags (
+    id INTEGER PRIMARY KEY,
+    label TEXT NOT NULL
+);
+
+CREATE TABLE card_tags (
+    card_id INTEGER NOT NULL REFERENCES cards(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    tag_id INTEGER NOT NULL REFERENCES tags(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    PRIMARY KEY (card_id, tag_id)
 );
