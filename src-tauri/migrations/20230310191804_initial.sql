6f2578eac30504b5304bd44d5750a4357f21e083
Cabidge <cabbageboi69@gmail.com>
2023-03-19 16:45:34 -0400

    +update database to include "dealers" for studying

--- src-tauri/migrations/20230310191804_initial.sql
+++ src-tauri/migrations/20230310191804_initial.sql
@@ -21,3 +21,44 @@ CREATE TABLE card_tags (
 ) WITHOUT ROWID;
 
 CREATE INDEX tag_index ON card_tags (tag);
+
+CREATE TABLE dealers (
+    id INTEGER NOT NULL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE filters (
+    id INTEGER NOT NULL PRIMARY KEY,
+    label TEXT NOT NULL,
+    pack_id INTEGER NOT NULL REFERENCES packs(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE included_tags (
+    filter_id INTEGER NOT NULL REFERENCES filters(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    tag TEXT NOT NULL COLLATE NOCASE,
+    PRIMARY KEY (filter_id, tag)
+) WITHOUT ROWID;
+
+
+CREATE TABLE excluded_tags (
+    filter_id INTEGER NOT NULL REFERENCES filters(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    tag TEXT NOT NULL COLLATE NOCASE,
+    PRIMARY KEY (filter_id, tag)
+) WITHOUT ROWID;
+
+CREATE TABLE dealer_filters (
+    dealer_id INTEGER NOT NULL REFERENCES dealers(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    filter_id INTEGER REFERENCES filters(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    strength INTEGER NOT NULL,
+    PRIMARY KEY (dealer_id, filter_id)
+);
