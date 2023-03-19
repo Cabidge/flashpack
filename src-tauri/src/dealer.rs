fd44ac99a9b60569799d46508fe204b4607644e6
Cabidge <cabbageboi69@gmail.com>
2023-03-19 19:43:28 -0400

    +dealer database functions

--- src-tauri/src/dealer.rs
+++ src-tauri/src/dealer.rs
@@ -1,4 +1,6 @@
-use crate::filter;
+use sqlx::{SqlitePool, FromRow};
+
+use crate::{filter, prelude::*};
 
 pub struct Summary {
     id: Id,
@@ -17,3 +19,43 @@ pub struct DealerFilter {
 }
 
 pub type Id = i64;
+
+pub async fn create(
+    pool: &SqlitePool,
+    title: &str,
+) -> Result<Id> {
+    #[derive(FromRow)]
+    struct InsertResult {
+        id: Id,
+    }
+
+    let row = sqlx::query_as!(
+        InsertResult,
+        "
+        INSERT INTO dealers (title)
+        VALUES (?)
+        RETURNING id
+        ",
+        title,
+    )
+    .fetch_one(pool)
+    .await?;
+
+    Ok(row.id)
+}
+
+pub async fn add_filter(pool: &SqlitePool, dealer_id: Id, filter_id: filter::Id, weight: i32) -> Result<()> {
+    sqlx::query!(
+        "
+        INSERT INTO dealer_filters (dealer_id, filter_id, strength)
+        VALUES (?, ?, ?)
+        ",
+        dealer_id,
+        filter_id,
+        weight,
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
