0221af9a294e81df8b5f33e014dad7b566faed70
Cabidge <cabbageboi69@gmail.com>
2023-03-19 19:38:16 -0400

    +expand filter database functions

--- src-tauri/src/filter.rs
+++ src-tauri/src/filter.rs
@@ -1,3 +1,7 @@
+use sqlx::{FromRow, SqlitePool};
+
+use crate::prelude::*;
+
 pub struct Summary {
     id: Id,
     label: String,
@@ -10,3 +14,67 @@ pub struct Filter {
 }
 
 pub type Id = i64;
+
+pub async fn create(
+    pool: &SqlitePool,
+    pack_id: crate::pack::Id,
+    label: &str,
+) -> Result<Id> {
+    #[derive(FromRow)]
+    struct InsertResult {
+        id: Id,
+    }
+
+    let row = sqlx::query_as!(
+        InsertResult,
+        "
+        INSERT INTO filters (label, pack_id)
+        VALUES (?, ?)
+        RETURNING id
+        ",
+        label,
+        pack_id,
+    )
+    .fetch_one(pool)
+    .await?;
+
+    Ok(row.id)
+}
+
+pub async fn add_included(
+    pool: &SqlitePool,
+    filter_id: Id,
+    tag: &str,
+) -> Result<()> {
+    sqlx::query!(
+        "
+        INSERT INTO included_tags (filter_id, tag)
+        VALUES (?, ?)
+        ",
+        filter_id,
+        tag,
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
+
+pub async fn add_excluded(
+    pool: &SqlitePool,
+    filter_id: Id,
+    tag: &str,
+) -> Result<()> {
+    sqlx::query!(
+        "
+        INSERT INTO excluded_tags (filter_id, tag)
+        VALUES (?, ?)
+        ",
+        filter_id,
+        tag,
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
