39c45b854d4dc9194ae3c9001dc8ab745278f576
Cabidge <cabbageboi69@gmail.com>
2023-03-19 21:03:04 -0400

    +select_card function for randomly selecting card based on filter

--- src-tauri/src/filter.rs
+++ src-tauri/src/filter.rs
@@ -1,6 +1,6 @@
 use sqlx::{FromRow, SqlitePool};
 
-use crate::prelude::*;
+use crate::{prelude::*, card};
 
 pub struct Summary {
     id: Id,
@@ -11,6 +11,12 @@ pub struct Filter {
     label: String,
     included_tags: Vec<String>,
     excluded_tags: Vec<String>,
+}
+
+#[derive(FromRow)]
+pub struct Prompt {
+    pub question: String,
+    pub answer: String,
 }
 
 pub type Id = i64;
@@ -77,4 +83,53 @@ pub async fn add_excluded(
     .await?;
 
     Ok(())
+}
+
+pub async fn select_card(
+    pool: &SqlitePool,
+    filter_id: Id,
+) -> Result<Option<card::Id>> {
+    #[derive(FromRow)]
+    struct QueryResult {
+        id: card::Id,
+    }
+
+    let row = sqlx::query_as!(
+        QueryResult,
+        r#"
+        SELECT c.id as "id!"
+        FROM cards c, filters f
+        WHERE c.pack_id = f.pack_id
+        AND f.id = ?
+        AND c.id IN (
+            WITH included AS (
+                SELECT it.tag
+                FROM included_tags it
+                WHERE it.filter_id = f.id
+            )
+            SELECT ct.card_id
+            FROM card_tags ct
+            WHERE ct.tag IN included
+            GROUP BY ct.card_id
+            HAVING COUNT(*) = (SELECT COUNT(*) FROM included)
+        )
+        AND c.id IN (
+            SELECT ct.card_id
+            FROM card_tags ct
+            WHERE ct.tag IN (
+                SELECT et.tag
+                FROM excluded_tags et
+                WHERE et.filter_id = f.id
+            )
+            GROUP BY ct.card_id
+        )
+        ORDER BY RANDOM()
+        LIMIT 1
+        "#,
+        filter_id,
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    Ok(row.map(|card| card.id))
 }
