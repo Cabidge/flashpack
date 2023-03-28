0f4ea029442ccab7ecc16810be8237ed92ed8007
Cabidge <cabbageboi69@gmail.com>
2023-03-28 18:30:52 -0400

    ~restyle prompt view

--- src/routes/study/[id]/practice/+page.svelte
+++ src/routes/study/[id]/practice/+page.svelte
@@ -35,20 +35,24 @@
         {#if prompt === undefined}
             <p>Unable to generate prompt...</p>
         {:else}
-            <h2>
+            <div>
                 {prompt.question}
-                {#if prompt.tags.length > 0}
-                    <ul class="flex flex-row gap-2 pt-2">
-                        {#each prompt.tags as tag (tag)}
-                            <li class="rounded-full bg-slate-100 px-2 text-sm">{tag}</li>
-                        {/each}
-                    </ul>
-                {/if}
-            </h2>
+            </div>
+
+            {#if prompt.tags.length > 0}
+                <ul class="flex flex-row gap-2 pt-2">
+                    {#each prompt.tags as tag (tag)}
+                        <li class="rounded-full bg-slate-100 px-2 text-sm">{tag}</li>
+                    {/each}
+                </ul>
+            {/if}
 
             {#if showAnswer}
                 <hr class="my-2" />
-                <p>{prompt.answer}</p>
+
+                <div>
+                    {prompt.answer}
+                </div>
             {/if}
         {/if}
     </Transition>
