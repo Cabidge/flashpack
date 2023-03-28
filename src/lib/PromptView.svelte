33b5d51cee51d206882f02522b33444f65c3125a
Cabidge <cabbageboi69@gmail.com>
2023-03-28 19:54:38 -0400

    ~extract component

---
+++ src/lib/PromptView.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type { Prompt } from "@bindings/Prompt";
+
+    export let prompt: Prompt;
+    export let showAnswer = true;
+</script>
+
+<div>
+    {prompt.question}
+</div>
+
+{#if prompt.tags.length > 0}
+    <ul class="flex flex-row gap-2 pt-2">
+        {#each prompt.tags as tag (tag)}
+            <li class="rounded-full bg-slate-100 px-2 text-sm">{tag}</li>
+        {/each}
+    </ul>
+{/if}
+
+{#if showAnswer}
+    <hr class="my-2" />
+
+    <div>
+        {prompt.answer}
+    </div>
+{/if}
