22bb6730a797155863dc9a2bda86193bb1b98f96
Cabidge <cabbageboi69@gmail.com>
2023-03-31 19:21:28 -0400

    ~restyle

--- src/routes/(pack)/card/[id]/RenameInput.svelte
+++ src/routes/(pack)/card/[id]/RenameInput.svelte
@@ -9,4 +9,4 @@
     export let placeholder = '';
 </script>
 
-<textarea draggable="false" rows="5" {placeholder} bind:value={inputValue} />
+<textarea class="rounded font-mono p-1 border border-slate-300 shadow" draggable="false" rows="5" {placeholder} bind:value={inputValue} />
