221534cd065451d8872c8b17674fb0f273a67900
Cabidge <cabbageboi69@gmail.com>
2023-04-16 20:31:06 -0400

    +add showAnswer toggle for preview

--- src/routes/(pack)/card/[id]/preview/+page.svelte
+++ src/routes/(pack)/card/[id]/preview/+page.svelte
@@ -6,13 +6,20 @@
     export let data: PageData;
 
     $: ({ id, card, prompt } = data);
+
+    let showAnswer = false;
 </script>
 
 <a href="/pack/{card.pack_id}">Return to Pack</a>
 <a href="/card/{id}">Edit</a>
 
+<label for="showAnswer">
+    <input type="checkbox" name="showAnswer" id="showAnswer" bind:checked={showAnswer}>
+    Show Answer
+</label>
+
 {#if card.tags.length > 0}
     <ShowTags tags={card.tags} />
 {/if}
 
-<PromptView {prompt} />
+<PromptView {prompt} {showAnswer} />
