365d61d5765a00da7d02cc97f3542f8dd08b8136
Cabidge <cabbageboi69@gmail.com>
2023-04-24 16:22:42 -0400

    ~modify ModalDialog styling and behavior

--- src/lib/ModalDialog.svelte
+++ src/lib/ModalDialog.svelte
@@ -2,9 +2,19 @@
     import { onMount } from 'svelte';
     import { setModalContext, type ModalContent, modals } from './modals';
     import { fade, fly } from 'svelte/transition';
-    import TagInput from './TagInput.svelte';
 
     export let content: ModalContent;
+
+    let closed = false;
+
+    const closeContent = () => {
+        if (closed) {
+            return;
+        }
+
+        closed = true;
+        content.close();
+    };
 
     let dialog: HTMLDialogElement;
 
@@ -13,20 +23,29 @@
     });
 
     setModalContext({
-        close: () => {
-            dialog.close();
-        }
+        close: () => closeContent(),
     });
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <dialog
     bind:this={dialog}
-    on:cancel|preventDefault={content.close}
-    on:close={() =>{
-        content.close();
-        dialog.showModal();
+    on:cancel|preventDefault={closeContent}
+    on:close={closeContent}
+    on:click|stopPropagation={(e) => {
+        let rect = dialog.getBoundingClientRect();
+        let isInDialog =
+            rect.top <= e.clientY &&
+            e.clientY <= rect.top + rect.height &&
+            rect.left <= e.clientX &&
+            e.clientX <= rect.left + rect.width;
+
+        if (!isInDialog) {
+            closeContent();
+        }
     }}
-    class="rounded shadow backdrop:fixed backdrop:inset-0 backdrop:bg-black backdrop:bg-opacity-50"
+    class="rounded shadow backdrop:fixed backdrop:inset-0 backdrop:bg-black backdrop:bg-opacity-50
+        {closed ? 'backdrop:hidden' : ''}"
     transition:fly={{ duration: 300, y: 10 }}
 >
     <svelte:component this={content.component} {...content.props} />
