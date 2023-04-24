b8f143ab0eef48b3bcfbb1daf57122795f59b272
Cabidge <cabbageboi69@gmail.com>
2023-04-24 13:27:54 -0400

    -remove Modal transition

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-    import { fade, fly } from 'svelte/transition';
-    import { expoOut } from 'svelte/easing';
-
     export let active = false;
     export let title: string;
 
@@ -13,12 +10,10 @@
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div
         on:click={close}
-        transition:fade={{ duration: 100 }}
         class="fixed left-0 top-0 z-10 flex h-screen w-screen items-center justify-center overflow-auto bg-black bg-opacity-50"
     >
         <div
             on:click|stopPropagation
-            transition:fly={{ y: 50, duration: 380, easing: expoOut }}
             class="overflow-hidden rounded bg-white shadow-lg"
         >
             <div class="relative w-full bg-slate-200 p-1 text-center text-lg font-semibold">
