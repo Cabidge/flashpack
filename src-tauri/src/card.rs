af621164ec017914605ecba4de51b232664c4476
Cabidge <cabbageboi69@gmail.com>
2023-05-02 17:47:23 -0400

    -removed unused import

--- src-tauri/src/card.rs
+++ src-tauri/src/card.rs
@@ -5,7 +5,7 @@ use serde::Serialize;
 use sqlx::SqlitePool;
 use ts_rs::TS;
 
-use crate::{pack, prelude::*};
+use crate::prelude::*;
 
 #[derive(TS, Serialize, Debug)]
 #[ts(export, export_to = "../src/bindings/")]
