4e19a2da14eeacc238379dfa9cde58e51bc3681d
Cabidge <cabbageboi69@gmail.com>
2023-06-10 17:41:24 +0800

    -remove reload method on packs and cards stores

--- src/lib/stores/cards.ts
+++ src/lib/stores/cards.ts
@@ -3,19 +3,16 @@ import { invoke } from '$lib/commands';
 import { createContext } from '$lib/context';
 import type { Card } from '@bindings/Card';
 import { listen } from '@tauri-apps/api/event';
-import { derived, writable, type Readable } from 'svelte/store';
+import { derived, type Readable } from 'svelte/store';
 
 export type CardWithId = Card & {
     id: number;
 };
 
 export const createStore = (packId: Readable<number>) => {
-    const trigger = writable(null);
-    const runTrigger = () => trigger.set(null);
-
     const cards = derived(
-        [packId, trigger],
-        ([$packId], set) => {
+        packId,
+        ($packId, set) => {
             const reloadCards = () => {
                 invoke('pack_cards', { id: $packId }).then((cardMap) => {
                     const cards = Object.entries(cardMap).map(([id, card]) => ({
@@ -39,8 +36,6 @@ export const createStore = (packId: Readable<number>) => {
         [] as CardWithId[]
     );
 
-    const reload = runTrigger;
-
     const get = (id: Readable<number>) =>
         derived([cards, id], ([$cards, $id]) => {
             return (
@@ -55,7 +50,6 @@ export const createStore = (packId: Readable<number>) => {
 
     return {
         ...cards,
-        reload,
         get
     };
 };
--- src/lib/stores/packs.ts
+++ src/lib/stores/packs.ts
@@ -1,42 +1,37 @@
 import { listen } from '@tauri-apps/api/event';
 import { invoke } from '$lib/commands';
 import type { Pack } from '@bindings/Pack';
-import { derived, writable, type Readable } from 'svelte/store';
+import { derived, readable, type Readable } from 'svelte/store';
 
 export type PackWithId = Pack & {
     id: number;
 };
 
 export type PacksStore = Readable<PackWithId[]> & {
-    reload: () => void;
     get: (id: Readable<number>) => Readable<Pack>;
 };
 
 const createStore = (): PacksStore => {
-    const trigger = writable(null);
-    const runTrigger = () => trigger.set(null);
+    const packs = readable([] as PackWithId[], (set) => {
+        const reload = () => {
+            invoke('pack_list').then((packMap) => {
+                const packs = Object.entries(packMap).map(([id, pack]) => ({
+                    id: Number(id),
+                    ...pack
+                }));
+                set(packs);
+            });
+        };
 
-    const packs = derived(
-        trigger,
-        (_$trigger, set) => {
-            const reloadPacks = () =>
-                invoke('pack_list').then((packs) =>
-                    set(Object.entries(packs).map(([id, pack]) => ({ id: Number(id), ...pack })))
-                );
+        reload();
 
-            reloadPacks();
+        const unlisten = listen('update:packs', reload);
 
-            const unlisten = listen('update:packs', reloadPacks);
-
-            return async () => {
-                // listen returns a Promise to a function that removes the listener
-                (await unlisten)();
-            };
-        },
-        [] as PackWithId[]
-    );
-
-    const reload = runTrigger;
+        return async () => {
+            // listen returns a Promise to a function that removes the listener
+            (await unlisten)();
+        };
+    });
 
     const get = (id: Readable<number>) =>
         derived([packs, id], ([$packs, $id]) => {
@@ -49,8 +44,6 @@ const createStore = (): PacksStore => {
 
     return {
         ...packs,
-        // Refetches the packs from the database
-        reload,
         // Creates a store pointing to the specific pack
         get
     };
