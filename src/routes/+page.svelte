f6bd03c7583dfeb89944ef422e619f5ee279ab8d
Cabidge <cabbageboi69@gmail.com>
2023-07-25 04:32:42 -0600

    add a create pack button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
+    import { invoke } from '$lib/commands';
+    import { modalStore } from '@skeletonlabs/skeleton';
+
     export let data;
 </script>
+
+<button
+    class="btn variant-filled"
+    on:click={() => {
+        modalStore.trigger({
+            type: 'prompt',
+            title: 'Create a Pack',
+            valueAttr: { type: 'text', placeholder: 'Title', required: true },
+            response: (title) => invoke('pack_create', { title })
+        });
+    }}
+>
+    Create a Pack
+</button>
 
 <nav class="list-nav">
     <ul>
