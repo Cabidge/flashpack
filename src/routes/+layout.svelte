0438c7b76582040579e9af178b9345b0e8c206ab
Cabidge <cabbageboi69@gmail.com>
2023-07-25 01:34:02 -0600

    add skeleton ui for styling
    thinking about styling while developing is a waste of time. custom styling can now be thought about later

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
+<script lang="ts">
+    // Your selected Skeleton theme:
+    import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+    // This contains the bulk of Skeletons required styles:
+    import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+    // Finally, your application's global stylesheet (sometimes labeled 'app.css')
+    import '../app.postcss';
+</script>
+
 <slot />
-
-<style global>
-    .katex {
-        font-size: 1em;
-    }
-
-    .katex-display {
-        font-size: 1.21em;
-    }
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-placeholder
+<script lang="ts">
+    let count = 0;
+</script>
+
+<button class="btn variant-filled" on:click={() => count++}>{count} clicks</button>
