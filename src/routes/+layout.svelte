735a5bb4b540d5284cc30b1bf112135ecbbe7cb8
Cabidge <cabbageboi69@gmail.com>
2023-07-25 02:03:57 -0600

    add automatic invalidation based on backend events

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,13 @@
 
     // Finally, your application's global stylesheet (sometimes labeled 'app.css')
     import '../app.postcss';
+
+    import { listen } from '@tauri-apps/api/event';
+    import { invalidate } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+    onMount(async () => await listen('update:packs', () => invalidate('flashpack:packs')));
+    onMount(async () => await listen('update:cards', () => invalidate('flashpack:cards')));
 </script>
 
 <slot />
