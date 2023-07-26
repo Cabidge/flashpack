034c30c23cd3b7de08d78a79856bceb08ed1a76b
Cabidge <cabbageboi69@gmail.com>
2023-07-26 00:44:58 -0600

    add app bar to root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,40 @@
 <script lang="ts">
     import { invoke } from '$lib/commands';
-    import { modalStore } from '@skeletonlabs/skeleton';
+    import { AppBar, modalStore } from '@skeletonlabs/skeleton';
 
     export let data;
 </script>
 
-<button
-    class="btn variant-filled"
-    on:click={() => {
-        modalStore.trigger({
-            type: 'prompt',
-            title: 'Create a Pack',
-            valueAttr: { type: 'text', placeholder: 'Title', required: true },
-            response: (title) => invoke('pack_create', { title })
-        });
-    }}
->
-    Create a Pack
-</button>
+<AppBar>
+    <h1 class="text-xl font-semibold">Flashpack</h1>
 
-<nav class="list-nav">
-    <ul>
-        {#each data.packs as pack (pack.id)}
-            <li>
-                <a href="/pack/{pack.id}">
-                    {pack.title}
-                </a>
-            </li>
-        {/each}
-    </ul>
-</nav>
+    <svelte:fragment slot="trail">
+        <button
+            class="chip variant-filled"
+            on:click={() => {
+                modalStore.trigger({
+                    type: 'prompt',
+                    title: 'Create a Pack',
+                    valueAttr: { type: 'text', placeholder: 'Title', required: true },
+                    response: (title) => invoke('pack_create', { title })
+                });
+            }}
+        >
+            Create a Pack
+        </button>
+    </svelte:fragment>
+</AppBar>
+
+<div class="p-4">
+    <nav class="list-nav">
+        <ul>
+            {#each data.packs as pack (pack.id)}
+                <li>
+                    <a href="/pack/{pack.id}">
+                        {pack.title}
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    </nav>
+</div>
