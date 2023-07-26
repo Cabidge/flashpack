6d0c1b9a67c899b48c84539d8d3171cfd8e7a3fa
Cabidge <cabbageboi69@gmail.com>
2023-07-26 14:10:19 +0800

    add a "add card" button

--- src/routes/pack/[id]/+page.svelte
+++ src/routes/pack/[id]/+page.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
+    import { invoke } from '$lib/commands.js';
+    import { modalStore } from '@skeletonlabs/skeleton';
+
     export let data;
 </script>
 
 <a href="/">Return to Packs</a>
 
 <h1>{data.pack.title}</h1>
+
+<button
+    class="btn variant-filled"
+    on:click={() => {
+        modalStore.trigger({
+            type: 'prompt',
+            title: 'Add a Card',
+            valueAttr: { type: 'text', placeholder: 'Label', required: true },
+            response: (label) => invoke('card_create', { packId: data.pack.id, label })
+        });
+    }}
+>
+    Add a Card
+</button>
 
 <nav class="list-nav">
     <ul>
