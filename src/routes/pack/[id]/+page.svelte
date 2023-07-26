b167b29a8771e4d77e17ae95b6b0debbee22256b
Cabidge <cabbageboi69@gmail.com>
2023-07-26 00:41:39 -0600

    add app bar to pack page

--- src/routes/pack/[id]/+page.svelte
+++ src/routes/pack/[id]/+page.svelte
@@ -1,36 +1,48 @@
 <script lang="ts">
     import { invoke } from '$lib/commands.js';
-    import { modalStore } from '@skeletonlabs/skeleton';
+    import { AppBar, modalStore } from '@skeletonlabs/skeleton';
 
     export let data;
 </script>
 
-<a href="/">Return to Packs</a>
+<AppBar>
+    <svelte:fragment slot="lead">
+        <a href="/">
+            <i class="fa-solid fa-arrow-left fa-lg" />
+        </a>
+    </svelte:fragment>
 
-<h1>{data.pack.title}</h1>
+    <h1 class="text-xl font-semibold">
+        {data.pack.title}
+    </h1>
 
-<button
-    class="btn variant-filled"
-    on:click={() => {
-        modalStore.trigger({
-            type: 'prompt',
-            title: 'Add a Card',
-            valueAttr: { type: 'text', placeholder: 'Label', required: true },
-            response: (label) => invoke('card_create', { packId: data.pack.id, label })
-        });
-    }}
->
-    Add a Card
-</button>
+    <svelte:fragment slot="trail">
+        <button
+            class="chip variant-filled"
+            on:click={() => {
+                modalStore.trigger({
+                    type: 'prompt',
+                    title: 'Add a Card',
+                    valueAttr: { type: 'text', placeholder: 'Label', required: true },
+                    response: (label) => invoke('card_create', { packId: data.pack.id, label })
+                });
+            }}
+        >
+            Add a Card
+        </button>
+    </svelte:fragment>
+</AppBar>
 
-<nav class="list-nav">
-    <ul>
-        {#each data.cards as card (card.id)}
-            <li>
-                <a href="/card/{card.id}">
-                    {card.label}
-                </a>
-            </li>
-        {/each}
-    </ul>
-</nav>
+<div class="p-4">
+    <nav class="list-nav">
+        <ul>
+            {#each data.cards as card (card.id)}
+                <li>
+                    <a href="/card/{card.id}">
+                        {card.label}
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    </nav>
+</div>
