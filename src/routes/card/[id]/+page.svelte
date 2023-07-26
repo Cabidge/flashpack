299bf45a1a4c4ceca1328f533841109b11dba76c
Cabidge <cabbageboi69@gmail.com>
2023-07-26 00:36:40 -0600

    add app bar to card page and extra styling

--- src/routes/card/[id]/+page.svelte
+++ src/routes/card/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { invoke } from '$lib/commands.js';
+    import { AppBar } from '@skeletonlabs/skeleton';
     import { onMount } from 'svelte';
 
     export let data;
@@ -30,20 +31,32 @@
     };
 </script>
 
-<a href="/pack/{data.card.pack_id}">Return to Pack</a>
+<AppBar>
+    <svelte:fragment slot="lead">
+        <a href="/pack/{data.card.pack_id}">
+            <i class="fa-solid fa-arrow-left fa-lg" />
+        </a>
+    </svelte:fragment>
 
-<h1>{data.card.label}</h1>
+    <h1 class="text-xl font-semibold">
+        {data.card.label}
+    </h1>
 
-{#if canSave}
-    <button class="chip variant-filled" on:click={saveChanges}>Save</button>
-{/if}
+    <svelte:fragment slot="trail">
+        {#if canSave}
+            <button class="chip variant-filled" on:click={saveChanges}>Save</button>
+        {/if}
+    </svelte:fragment>
+</AppBar>
 
-<label class="label">
-    <span>Script</span>
-    <textarea class="textarea font-mono" rows={8} bind:value={script} />
-</label>
+<div class="p-4">
+    <label class="label">
+        <span>Script</span>
+        <textarea class="textarea font-mono" rows={8} bind:value={script} />
+    </label>
 
-<label class="label">
-    <span>Template</span>
-    <textarea class="textarea font-mono" rows={8} bind:value={template} />
-</label>
+    <label class="label">
+        <span>Template</span>
+        <textarea class="textarea font-mono" rows={8} bind:value={template} />
+    </label>
+</div>
