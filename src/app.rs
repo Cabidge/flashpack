5e79cb8d319f20e7214d0f01c407032814b0b17d
Wenhao Dong <cabbageboi69@gmail.com>
2024-01-16 13:57:43 -0500

    Begin working on card slides

--- src/app.rs
+++ src/app.rs
@@ -1,36 +1,51 @@
 use leptos::*;
-use serde::Serialize;
-
-use crate::commands::invoke;
 
 #[component]
 pub fn App() -> impl IntoView {
-    #[derive(Serialize)]
-    struct Args {
-        name: String,
-    }
-
-    let greetings = ["foo", "bar", "baz"]
-        .into_iter()
-        .map(|x| {
-            let greeting = create_resource(
-                || (),
-                move |_| {
-                    let name = String::from(x);
-                    let args = Args { name };
-                    async move { invoke::<String>("greet", &args).await.unwrap() }
-                },
-            );
-
-            view! {
-                <p>{move || greeting.get()}</p>
-            }
-        })
-        .collect_view();
+    let card_slides = vec![
+        String::from("Hello"),
+        String::from("Foo"),
+        String::from("Bar"),
+    ];
 
     view! {
         <main>
-            {greetings}
+            <Card card_slides/>
         </main>
+    }
+}
+
+#[component]
+fn Card(card_slides: Vec<String>) -> impl IntoView {
+    let (visible_count, set_visible_count) = create_signal(1);
+    let slide_count = card_slides.len();
+
+    let advance_slides = move || set_visible_count.update(|count| *count += 1);
+
+    let visible_cards = move || {
+        card_slides
+            .iter()
+            .take(visible_count.get())
+            .map(|slide| {
+                view! {
+                    <li>
+                        <div inner_html=slide></div>
+                    </li>
+                }
+            })
+            .collect_view()
+    };
+
+    view! {
+        <ul>
+            {visible_cards}
+            <Show when=move || visible_count.get() < slide_count>
+                <li>
+                    <button on:click=move |_| advance_slides()>
+                        "Next"
+                    </button>
+                </li>
+            </Show>
+        </ul>
     }
 }
