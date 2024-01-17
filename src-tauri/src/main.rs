6ffd9a1a0b95f710975a2d3a895910ce122aebbb
Wenhao Dong <cabbageboi69@gmail.com>
2024-01-16 19:22:50 -0500

    Add add_card command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,8 +6,36 @@ use std::{path::PathBuf, sync::Mutex};
 use tauri::{api::dialog::blocking::FileDialogBuilder, State};
 
 struct Collection(PathBuf);
+struct Pack(PathBuf);
+struct Card(PathBuf);
 
 type CollectionState = Mutex<Option<Collection>>;
+
+impl Collection {
+    fn pack(&self, pack_name: &str) -> Pack {
+        Pack(self.0.join(pack_name))
+    }
+}
+
+impl Pack {
+    fn open_card(self, card_name: &str) -> Card {
+        let mut card_path = self.0;
+        card_path.push(card_name);
+        card_path.set_extension(CARD_EXTENSION);
+        Card(card_path)
+    }
+}
+
+impl Card {
+    fn set_contents(&self, contents: &str) -> std::io::Result<()> {
+        if let Some(pack_path) = self.0.parent() {
+            std::fs::create_dir_all(pack_path)?;
+        }
+        std::fs::write(&self.0, contents)
+    }
+}
+
+const CARD_EXTENSION: &str = "flashmark";
 
 #[tauri::command]
 async fn open_collection(collection: State<'_, CollectionState>) -> Result<Option<String>, ()> {
@@ -51,10 +79,32 @@ fn list_packs(collection: State<CollectionState>) -> Vec<String> {
         .collect()
 }
 
+#[tauri::command]
+fn add_card(
+    collection: State<CollectionState>,
+    pack_name: String,
+    card_name: String,
+    contents: String,
+) {
+    let mut collection = collection.lock().unwrap();
+    let Some(collection) = collection.as_mut() else {
+        return;
+    };
+
+    let card = collection.pack(&pack_name).open_card(&card_name);
+
+    // TODO: error handling
+    card.set_contents(&contents).unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
         .manage(CollectionState::default())
-        .invoke_handler(tauri::generate_handler![open_collection, list_packs])
+        .invoke_handler(tauri::generate_handler![
+            open_collection,
+            list_packs,
+            add_card
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
