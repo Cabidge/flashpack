2c4ae50df329a46a28ceeb8dbe70f8252d35593d
Wenhao Dong <cabbageboi69@gmail.com>
2024-01-20 17:35:02 -0500

    Refactor invoke module so command definitions are at the top

--- src/invoke.rs
+++ src/invoke.rs
@@ -3,6 +3,14 @@ use std::collections::BTreeSet;
 use serde::{de::DeserializeOwned, Serialize};
 use wasm_bindgen::prelude::*;
 
+macros::define_commands! {
+    open_collection() -> Option<String>;
+    list_packs() -> BTreeSet<String>;
+    list_cards(pack_name: String) -> BTreeSet<String>;
+    add_card(pack_name: String, card_name: String, contents: String);
+    get_card(pack_name: String, card_name: String) -> Option<String>;
+}
+
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"], js_name = invoke)]
@@ -18,40 +26,37 @@ pub async fn invoke<T: DeserializeOwned>(
     serde_wasm_bindgen::from_value(result)
 }
 
-macro_rules! define_command {
-    ($name:ident($($arg_name:ident: $arg_ty:ty),*) $(-> $ret:ty)?) => {
-        pub async fn $name($($arg_name: $arg_ty),*) $(-> $ret)? {
-            camelpaste::paste! {
-                #[derive(serde::Serialize)]
-                #[allow(non_snake_case)]
-                struct Args {
-                    $( [<$arg_name:camel>]: $arg_ty, )*
+mod macros {
+    macro_rules! define_command {
+        ($name:ident($($arg_name:ident: $arg_ty:ty),*) $(-> $ret:ty)?) => {
+            pub async fn $name($($arg_name: $arg_ty),*) $(-> $ret)? {
+                camelpaste::paste! {
+                    #[derive(serde::Serialize)]
+                    #[allow(non_snake_case)]
+                    struct Args {
+                        $( [<$arg_name:camel>]: $arg_ty, )*
+                    }
+
+                    let args = Args {
+                        $( [<$arg_name:camel>]: $arg_name, )*
+                    };
                 }
 
-                let args = Args {
-                    $( [<$arg_name:camel>]: $arg_name, )*
-                };
+                crate::invoke::invoke(stringify!($name), &args).await.unwrap()
             }
-
-            invoke(stringify!($name), &args).await.unwrap()
         }
     }
-}
 
-macro_rules! define_commands {
-    ($($name:ident($($args:tt)*) $(-> $ret:ty)?);+ $(;)?) => {
-        $(
-            define_command! {
-                $name($($args)*) $(-> $ret)?
-            }
-        )+
+    macro_rules! define_commands {
+        ($($name:ident($($args:tt)*) $(-> $ret:ty)?);+ $(;)?) => {
+            $(
+                crate::invoke::macros::define_command! {
+                    $name($($args)*) $(-> $ret)?
+                }
+            )+
+        }
     }
-}
 
-define_commands! {
-    open_collection() -> Option<String>;
-    list_packs() -> BTreeSet<String>;
-    list_cards(pack_name: String) -> BTreeSet<String>;
-    add_card(pack_name: String, card_name: String, contents: String);
-    get_card(pack_name: String, card_name: String) -> Option<String>;
+    pub(crate) use define_command;
+    pub(crate) use define_commands;
 }
