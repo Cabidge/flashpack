b21faaa9b90f96e0e1ac660ec44492dfc287b10b
Wenhao Dong <cabbageboi69@gmail.com>
2024-01-23 20:41:28 -0500

    Add Result handling to invoke function

--- src/invoke.rs
+++ src/invoke.rs
@@ -15,23 +15,36 @@ macros::define_commands! {
 
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"], js_name = invoke)]
-    pub async fn invoke_js_value(cmd: &str, args: JsValue) -> JsValue;
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"], js_name = invoke, catch)]
+    pub async fn invoke_js_value(cmd: &str, args: JsValue) -> Result<JsValue, JsValue>;
 }
 
-pub async fn invoke<T: DeserializeOwned>(
+// this is such a goofy return type
+pub async fn try_invoke<T: DeserializeOwned, E: DeserializeOwned>(
     cmd: &str,
     args: &impl Serialize,
-) -> Result<T, serde_wasm_bindgen::Error> {
+) -> Result<Result<T, E>, serde_wasm_bindgen::Error> {
     let args = serde_wasm_bindgen::to_value(args)?;
-    let result = invoke_js_value(cmd, args).await;
-    serde_wasm_bindgen::from_value(result)
+
+    let result = match invoke_js_value(cmd, args).await {
+        Ok(val) => Ok(serde_wasm_bindgen::from_value(val)?),
+        Err(err) => Err(serde_wasm_bindgen::from_value(err)?),
+    };
+
+    Ok(result)
+}
+
+pub async fn invoke<T: DeserializeOwned, E: DeserializeOwned>(
+    cmd: &str,
+    args: &impl Serialize,
+) -> Result<T, E> {
+    try_invoke(cmd, args).await.unwrap()
 }
 
 mod macros {
     macro_rules! define_command {
-        ($name:ident($($arg_name:ident: $arg_ty:ty),*) $(-> $ret:ty)?) => {
-            pub async fn $name($($arg_name: $arg_ty),*) $(-> $ret)? {
+        ($name:ident($($arg_name:ident: $arg_ty:ty),*) -> Result<$ret:ty, $err:ty>) => {
+            pub async fn $name($($arg_name: $arg_ty),*) -> Result<$ret, $err> {
                 camelpaste::paste! {
                     #[derive(serde::Serialize)]
                     #[allow(non_snake_case)]
@@ -44,9 +57,23 @@ mod macros {
                     };
                 }
 
-                crate::invoke::invoke(stringify!($name), &args).await.unwrap()
+                crate::invoke::invoke(stringify!($name), &args).await
             }
-        }
+        };
+        ($name:ident($($arg_name:ident: $arg_ty:ty),*) -> $ret:ty) => {
+            pub async fn $name($($arg_name: $arg_ty),*) -> $ret {
+                crate::invoke::macros::define_command! {
+                    $name($($arg_name: $arg_ty),*) -> Result<$ret, ()>
+                }
+
+                $name($($arg_name),*).await.unwrap()
+            }
+        };
+        ($name:ident($($args:tt)*)) => {
+            crate::invoke::macros::define_command! {
+                $name($($args)*) -> ()
+            }
+        };
     }
 
     macro_rules! define_commands {
